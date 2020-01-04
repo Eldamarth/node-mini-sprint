efccebffc4fb2ef497eaec7b0f2f79eddac83eeb
James Kelley <james.a.kelley.mars@gmail.com>
2020-01-04 16:35:59 -0700

    Complete basic functionality of node-mini-sprint

--- client/main.js
+++ client/main.js
@@ -6,19 +6,44 @@ $(document).ready(function() {
 // when the user enters data and clicks submit, post the quote to the server
   $('#submit').click((e) => {
     e.preventDefault();
+    
     let quote = $('input').val();
     addQuote(quote);
+    
   });
+  $('#image').click( (e)=> {
+    e.preventDefault();
+
+    getQuote();
+  })
 
   function getQuote(){
-
     //YOUR CODE HERE, Add a GET request
+    // AJAX GET request $.get
 
+    fetch('http://localhost:3000/quote')
+      .then(data => data.text())
+      .then(result => $('#quote').html(result))
+
+    //  $.get("http://localhost:3000/quote", data => {
+    //    console.log("DATA: "+data);
+    //  }
+
+    //  );
   }
 
-  function addQuote(quote){
-    
+  function addQuote(quote, successCB = null, failureCB = null){
+    console.log(quote)
     //YOUR CODE HERE, Add a POST request
 
+    $.post(
+      "http://localhost:3000/quote",
+      
+      quote,
+
+      () => console.log(quote)
+    
+    )
+    .then(data => data.text())
   }
 });
--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
 const http = require('http');
+const _ = require('underscore');
+
+// handle buffer requests
+
 
 //headers to allows CORS requests
 const headers = {
@@ -12,11 +16,20 @@ const port = 3000;
 
 // TODO: Fill with strings of your favorite quotes :)
 const quotes = [
-  'one',
-  'two',
-  'three',
-  'four',
-  'five'
+  'Text bubbles are made out of flesh-eating bacteria. You didn\'t know that?',
+  'Isn\'t this the Hollywood Stock Exchange? You know, where you buy and sell celebrity stocks based on the ups and down of their careers?',
+  'You really haven\'t read my MySpace page, have you? You say you have, but you really haven\'t.',
+  'All I have to do is get married by next week and I can get my blender in time for Grey\'s Anatomy!',
+  'In nature, a horse will not offer you his hoof until you gain his trust.',
+  '*Gasp* I dropped my meatball in the pool!',
+  'What, no way! Wait, why is this wrong? Next to \'Miranda Rights,\' I wrote, \'Miranda has the right to a decent man who will help her raise her baby.\' Are these questions not about Sex and the City?',
+  'I\'m going to make you cry and dip my cookie in your tears.',
+  'Can\'t a guy go into a stress induced hibernation without getting thrown in a landfill?',
+  'I told you it\'s menthol. So it\'s healthier than an apple.',
+  'In the words of every sit-com character in the early \'90s and everyone in the Midwest through the rest of the \'90s, don\'t go there.',
+  'Well, then I\'m taking away fifty points from Gryffindor!!',
+  'Oh, my God! Stan has no friends! And he didn\'t even realize! He\'s like America, the guy.',
+  'All I ask in return is that you vote with me on the dish. Wait.  Did I make that up, or is that real? Is there even a vote? I don\'t know what\'s real.',
 ];
 
 //Utility Function to return a random integer
@@ -36,15 +49,47 @@ const handleRequest = function(req, res) {
     res.end();
   }
 
+
+
+
+
+
   // TODO: GET ONE
-  if ((req.url == '/quote/' || req.url == '/quote') && req.method == "FILL ME IN") {
+  if ((req.url == '/quote/' || req.url == '/quote') && req.method == "GET") {
     //YOUR CODE HERE
 
+    let quote = _.sample(quotes);
+    res.writeHead(200,headers);
+    res.end('"'+quote+'"');
   }
+
+
+
+
   // TODO: POST/CREATE
-  else if ((req.url == 'FILL ME IN' || req.url == 'FILL ME IN') && req.method == "FILL ME IN") {
+  // else if ((req.url == 'FILL ME IN' || req.url == 'FILL ME IN') && req.method == "POST") {
+  else if ((req.url == '/quote/' || req.url == '/quote') && req.method == "POST") {
     //YOUR CODE HERE
+    
+
+    var body = "";
+    req.on('data', function (chunk) {
+      body += chunk;
+    });
+    req.on('end', function () {
+      quotes.push(body);
+      console.log('POSTed: ' + body);
+      res.writeHead(200,headers);
+      res.end('Thank you for suggesting a Roger quote!');
+    });
+
+    
   }
+
+
+
+
+
 
 //CATCH ALL ROUTE
   else {
